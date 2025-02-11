7c574d61e746df7338febafdb17ac30685cc10b8
Kazike19 <kazike.mv@gmail.com>
2025-02-11 14:45:03 -0600

    Agrega funcion de listaDeAmigos

--- app.js
+++ app.js
@@ -9,11 +9,39 @@ function agregarAmigo() {
         alert("Por favor, inserte un nombre");
     }else{
         amigos.push(amigoNombre);
-        limpiarCaja()
+        limpiarCaja();
         console.log(amigos);
+        listaDeAmigos(amigos);
     }
 
     
+}
+
+function listaDeAmigos(amigosLista) {
+    let listahtml = document.getElementById("listaAmigos");
+    console.log(amigosLista.length);
+    listahtml.innerHTML = "";   
+    for(let i = 0; i < amigosLista.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigosLista[i];
+        listahtml.appendChild(li);
+        //document.querySelector('#listaAmigos').value = `hola <li>${amigosLista[i]}</li>`;
+    } 
+    
+
+
+    //document.querySelector('#listaAmigos').value = `${numeroUsuario}`;
+    //document.querySelector.
+
+    
+}
+
+
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
 
 function sortearAmigo(){
@@ -22,4 +50,5 @@ function sortearAmigo(){
 
 function limpiarCaja(){
     document.querySelector('#amigo').value = '';
+
 }
