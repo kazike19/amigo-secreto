afab7ce43abdd2e6f87f159fba9a0b2e71f59ea8
Kazike19 <kazike.mv@gmail.com>
2025-02-11 13:41:15 -0600

    Agrega funciones agrega amigo, sortear amigo y limpiar caja

---
+++ README.md
@@ -0,0 +1,3 @@
+#amigo secreto
+
+<a href= "https://github.com/kazike19/amigo-secreto.git">
--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-
+let amigos = [];
 
 
 function agregarAmigo() {
+    amigoNombre = document.getElementById('amigo').value;
+    if(amigoNombre == 0){
+        alert("Por favor, inserte un nombre");
+    }else{
+        amigos.push(amigoNombre);
+        limpiarCaja()
+        console.log(amigos);
+    }
+
     
 }
 
 function sortearAmigo(){
 
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
 }
