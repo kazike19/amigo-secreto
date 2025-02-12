9bdf2c9551668c6031a14cb7cccb29473346a810
Kazike19 <kazike.mv@gmail.com>
2025-02-11 23:30:21 -0600

    Agrega funcion sortearAmigo Terminada

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = [];
+const listahtml = document.getElementById("listaAmigos");
 
-
+// Funcion para agregar un amigo a la lista desde el elemento input
 function agregarAmigo() {
+
     amigoNombre = document.getElementById('amigo').value;
+
     if(amigoNombre == 0){
         alert("Por favor, inserte un nombre");
     }else{
@@ -17,38 +20,43 @@ function agregarAmigo() {
     
 }
 
+
+// Funcion para seleccionar la lista del html, luego limpia el contenido antes de agregar los valores que existen dentro de la lista amigos
 function listaDeAmigos(amigosLista) {
-    let listahtml = document.getElementById("listaAmigos");
+    
     console.log(amigosLista.length);
     listahtml.innerHTML = "";   
+    
     for(let i = 0; i < amigosLista.length; i++) {
         let li = document.createElement("li");
-        li.textContent = amigosLista[i];
+        li.innerHTML = `${amigos[i]}`;
         listahtml.appendChild(li);
-        //document.querySelector('#listaAmigos').value = `hola <li>${amigosLista[i]}</li>`;
     } 
     
-
-
-    //document.querySelector('#listaAmigos').value = `${numeroUsuario}`;
-    //document.querySelector.
-
-    
 }
 
 
-
-function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
-
+// Funcion para sortear al amigo ganador, tomando en cuenta la longitud de la lista, luego elije uno al azar y finalmente lo muestra en el html con diferente color
 function sortearAmigo(){
+    const longitudLista = amigos.length;
+    let numeroGenerado = Math.floor(Math.random()*longitudLista);
+    let li = document.createElement("li"); 
+
+    if(longitudLista > 1){
+
+        listahtml.innerHTML = "";
+        li.innerHTML = `El amigo secreto es: ${amigos[numeroGenerado]}`;
+        li.style.color = "green";
+        listahtml.appendChild(li);
+
+    }else{
+        alert("Debe haber al menos dos nombres en la lista");
+    }
 
 }
 
+
+// FUncion para limpiar la caja del input
 function limpiarCaja(){
     document.querySelector('#amigo').value = '';
-
 }
